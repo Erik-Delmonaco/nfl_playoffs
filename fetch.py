ac37bdc0fe2d38a6e47c5774cbcc0e08c3d6796c
Erik-Delmonaco <delmonacoerik@gmail.com>
2026-01-21 16:17:32 +0000

    completed class exercise

--- fetch.py
+++ fetch.py
@@ -2,25 +2,23 @@ import sqlite3
 import gradio as gr
 import pandas as pd
 
-def fetch_teams():
+def fetch():
     conn = sqlite3.connect('teams_database.db')
     cursor = conn.cursor()
+
     query = """
         SELECT *
         FROM teams;
-
     """
     cursor.execute(query)
-
-    results = cursor.fetchall()
-
+    records = cursor.fetchall()
     conn.close()
 
-    df = pd.DataFrame(results,columns = ["id","city","name"])
+    points = [(len(record[1]),len(record[2])) for record in records]
+    points_df = pd.DataFrame(points,columns = ('x','y'))
+    return points_df
 
-    return df
 
-iface = gr.Interface(
-    fn = fetch_teams,
-    inputs = []
-)
+iface = gr.Interface(fn = fetch,inputs = [], outputs = gr.LinePlot(x = 'x',y = 'y',x_lim = (0,15),y_lim = (0,15)))
+
+iface.launch()
